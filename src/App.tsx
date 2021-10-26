010b71ce5c49dcffda94e6f1c180b7ab677c66d2
Russell Chien <RussellChien6@gmail.com>
2021-10-25 23:12:20 -0700

    update files

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import styles from './App.module.css';
 import { ChangeEvent, useState } from "react";
 import 'katex/dist/katex.min.css';
 import Latex from 'react-latex-next';
+import evaluatex from 'evaluatex/dist/evaluatex';
 
 export default function App() {
   const [expr, setExpr] = useState('');
   return (
+
     <div className={styles.header}>
       <h1 style={{color: 'White'}}>Tex Evaluator</h1>
       <p>This is my first React app!</p>
@@ -14,7 +16,7 @@ export default function App() {
         <h2>Evaluate Tex Expression</h2>
         <TextField variant="filled" label="Enter Tex Expression here" onChange={handleInputChange} />       
         <p><Latex>{`$$${expr}$$`}</Latex></p>
-        <p>Expression Value Goes Here</p>
+        <p><p>{evaluate(expr) ?? ''}</p></p>
     </Card>
     </div>
   );
@@ -22,5 +24,13 @@ export default function App() {
   function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
     const input = event.target.value;
     setExpr(input);
+  }
+
+  function evaluate(texExpr: string): number | undefined {
+    try {
+      return evaluatex(texExpr)();
+    } catch (error) {
+      return undefined;
+    }
   }
 }
